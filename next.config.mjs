f84fab3b629f6d767ce2ba7498b1a931f8410f35
Энхтүвшин Эрхэмбаяр <HP239412@G302-CC0154.local>
2024-04-11 19:39:55 +0800

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL || "",
+    OTP_SECRET: OTP_SECRET,
+    JWT_SECRET: JWT_SECRET,
+    EMAIL_USER: "" || EMAIL_USER,
+    EMAIL_PASSWORD: "eyvz vtto vcux slzq",
+  },
+};
 
 export default nextConfig;
--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -2,11 +2,11 @@ import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { ApolloServer } from "@apollo/server";
 import { typeDefs } from "@/graphql/schemas";
 import { resolvers } from "@/graphql/resolvers";
-import { getCurrentUser } from "@/services/auth.service";
 
 const server = new ApolloServer({
   resolvers,
   typeDefs,
+  introspection: true,
 });
 
 const handler = startServerAndCreateNextHandler(server);
